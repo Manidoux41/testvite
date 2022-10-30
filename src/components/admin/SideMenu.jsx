178b3823b728664132b3e814d52c4f2b382503b3
superflyman90 <superflyman90@gmail.com>
2022-10-30 17:29:14 +0100

    sylising admin pages

--- src/components/admin/SideMenu.jsx
+++ src/components/admin/SideMenu.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SideMenu = () => {
   return (
     <div className='SideMenu'>
-
+      Sidemenu
     </div>
   )
 }
--- src/pages/Admin/ALayout.jsx
+++ src/pages/Admin/ALayout.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
+import Header from '../../components/admin/Header'
+import SideMenu from '../../components/admin/SideMenu'
+
 const ALayout = () => {
   return (
     <div className='ALayout'>
-        Layout
-        <Outlet />
+        <Header />
+        <div id="admin">
+          <SideMenu />
+          <div id="admin__body">
+            <Outlet />
+          </div>
+        </div>
     </div>
   )
 }
