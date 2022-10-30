68e0990da754c17c191a38f86ad722f39215eebc
superflyman90 <superflyman90@gmail.com>
2022-10-30 12:20:20 +0100

    add pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import Logo from './assets/logo.png'
+import Home from './pages/Home'
+import Services from './pages/Services'
+import Portfolio from './pages/Portfolio'
+import Contact from './pages/Contact'
+
 
 const App = () => {
   return (
     <div className="App">
       <h1>My Portfolio</h1>
       <img src={Logo} alt="my logo" />
+      <Home />
+      <Services />
+      <Portfolio />
+      <Contact />
     </div>
     )
 }
---
+++ src/pages/Services.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <div>
+        <h2>Service Works</h2>
+    </div>
+  )
+}
+
+export default Services
