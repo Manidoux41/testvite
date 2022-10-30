ceeab0743c88203ebbbf61bc8053843bfee7f81c
superflyman90 <superflyman90@gmail.com>
2022-10-30 18:05:57 +0100

    test menu admin3

--- src/pages/Admin/AdminRouter.jsx
+++ src/pages/Admin/AdminRouter.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 
 import { ALayout, Dashboard } from '../../pages/Admin'
+import Error from '../../utils/Error'
 
 const AdminRouter = () => {
   return (
@@ -11,6 +12,8 @@ const AdminRouter = () => {
             <Route index element={<Dashboard />} />
 
             <Route path='dashboard' element={<Dashboard />}/>
+            <Route path='*' element={<Error />}/>
+
           </Route>
     </Routes>
   )
