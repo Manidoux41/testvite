edf64936d57bd3a8e20adc65a38f8257b079c7a8
superflyman90 <superflyman90@gmail.com>
2022-10-30 12:08:35 +0100

    adding logo png

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,13 @@
-import { useState } from 'react'
-import Logo from './assets/safari-pinned-tab.svg
+import React from 'react'
+import Logo from './assets/logo.png'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={Logo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>My Portfolio</h1>
+      <img src={Logo} alt="my logo" />
     </div>
-  )
+    )
 }
 
 export default App
